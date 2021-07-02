b827a7522bdcdfa0eb06ba64737468d43dd8c874
Amaka-A <77367947+Amaka-A@users.noreply.github.com>
2021-07-01 19:03:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
-car1_width= 120;
-car1_height=70;
-car1_image="carSprite.png";
-car1_x=10;
-car1_y=10;
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://i.postimg.cc/9rqYz9HM/car1.png";
+car1_x = 10;
+car1_y = 10;
 
-car2_width=120;
-car2_height=70;
-car2_image="carSprite2.png";
-car2_x=10;
-car2_y=10;
+car2_width = 120;
+car2_height = 70;
+car2_image = "https://i.postimg.cc/tnnW1Kff/car2.png";
+car2_x = 10;
+car2_y = 100;
 
-background_image = "racing.jpg";
-
-rover_image = "rover.png";
-
-rover_x = 10;
-rover_y = 10;
+background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
 
 function add() {
 	background_imgTag = new Image(); //defining a variable with a new image
@@ -28,14 +23,21 @@ function add() {
 	car1_imgTag = new Image(); //defining a variable with a new image
 	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
 	car1_imgTag.src = car1_image;   // load image
+
+	car2_imgTag = new Image(); //defining a variable with a new image
+	car2_imgTag.onload = uploadcar2; // setting a function, onloading this variable
+	car2_imgTag.src = car2_image;   // load image
 }
 
 function uploadBackground() {
 	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
-function uploadrover() {
+function uploadcar1() {
 	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+}
+function uploadcar2() {
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
 }
 
 
@@ -64,22 +66,21 @@ function my_keydown(e)
 		{
 			car1_right();
 			console.log("right arrow key");
-		
 		}
 		if(keyPressed == '87')
 		{
 			car2_up();
 			console.log("key w");
 		}
-		if(keyPressed == '65')
-		{
-			car2_down();
-			console.log("key a");
-		}
 		if(keyPressed == '83')
 		{
-			car2_left();
+			car2_down();
 			console.log("key s");
+		}
+		if(keyPressed == '65')
+		{
+			car2_left();
+			console.log("key a");
 		}
 		if(keyPressed == '68')
 		{
